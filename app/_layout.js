c9615f5138c3fc81498c27ee7b5fce9d368ea893
abhishek Hegde <ahegde2021@gmail.com>
2023-11-09 19:12:17 +0530

    fonts added in layout

--- app/_layout.js
+++ app/_layout.js
@@ -1,9 +1,26 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { useCallback } from "react";
+import * as SplashScreen from "expo-splash-screen";
 
-const Layout = ()=>{
-    return(
-        <Stack/>
+SplashScreen.preventAutoHideAsync();
+
+const Layout = () => {
+    const [fontsLoaded] = useFonts({
+      DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+      DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+      DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+    });
+  
+    if (!fontsLoaded) {
+      return null;
+    }
+  
+    return (
+      <Stack initialRouteName="home">
+        <Stack.Screen name="home" />
+      </Stack>
     )
-}
-
-export default Layout;
+  };
+  
+  export default Layout;
