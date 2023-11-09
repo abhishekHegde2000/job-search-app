5a46045f88c4b4f7b8638e38563632dbfe62c56d
abhishek Hegde <ahegde2021@gmail.com>
2023-11-09 18:55:56 +0530

    headerleft headerright added

--- app/index.js
+++ app/index.js
@@ -1,16 +1,36 @@
-import {View , Text , ScrollView , SafeAreaView} from 'react-native';
-import { useState } from 'react';
-import { Stack,useRouter } from 'expo-router';
-import { COLORS,icons,images,SIZES } from '../constants';
-import {Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome} from '../components';
-
-const Home = ()=>{
+import { View, SafeAreaView , Text , ScrollView} from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+import { COLORS , icons, images , SIZES } from '../constants';
+import { Nearbyjobs , Popularjobs, ScreenHeaderBtn , Welcome } from '../components';
+import { useState , useEffect } from 'react';
+const Home = () => {
     const router = useRouter();
 
     return (
-        <SafeAreaView style = {{flex:1}}>
-            <Text>Home</Text>
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerLeft:()=>(
+                    <ScreenHeaderBtn iconUrl = {icons.menu} dimension = "60%" />
+                    ),
+                    headerRight:()=>(
+                    <ScreenHeaderBtn iconUrl = {images.profile} dimension = "100%" />
+                    ),
+                    headerTitle: "Home"
+                }}
+            />
+            <ScrollView showsVerticalScrollIndicator = {false} >
+                <View style={{ flex: 1, padding: SIZES.medium }}>
+                    <Welcome />
+                    <Popularjobs />
+                    <Nearbyjobs />
+                </View>
+
+            </ScrollView>
+
         </SafeAreaView>
-    )
-}
+    );
+};
 export default Home;
