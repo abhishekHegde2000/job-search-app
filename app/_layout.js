b403372a860240f12439a80c50369560be7b4e54
abhishek Hegde <ahegde2021@gmail.com>
2023-11-09 19:14:50 +0530

    fonsta loaded asnc function added

--- app/_layout.js
+++ app/_layout.js
@@ -1,26 +1,26 @@
 import { Stack } from "expo-router";
 import { useFonts } from "expo-font";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import * as SplashScreen from "expo-splash-screen";
-
-SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
     const [fontsLoaded] = useFonts({
-      DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
-      DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
-      DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+        DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
     });
-  
-    if (!fontsLoaded) {
-      return null;
-    }
-  
-    return (
-      <Stack initialRouteName="home">
-        <Stack.Screen name="home" />
-      </Stack>
-    )
-  };
-  
-  export default Layout;
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+
+    useEffect(() => {
+        onLayoutRootView();
+    }, [onLayoutRootView]);
+
+    return <Stack />;
+};
+
+export default Layout;
